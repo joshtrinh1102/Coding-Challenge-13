31683ca1def2b9f1c2c9c4c82e40752255ef7bfe
joshtrinh1102 <nltrinh1102@gmail.com>
2024-06-28 21:39:48 -0400

    Create navigation buttons for product display

--- example.js
+++ example.js
@@ -1,11 +1,36 @@
-function main(){
-    fetch ('https://course-api.com/react-store-products')
-    .then(res =>
-         res.json())
-    .then(data => {
-        console.log(data),
-        })
-    //fetch data
-    .catch(error => console.log(error));//catch error
+let count = 0;
+function call(count){
+    const request= fetch ('https://course-api.com/react-store-products')
+    .then((response)=>{console.log(response);
+        return response.json();
+    }).then((data)=>{
+            console.log(data);
+            document.querySelector('#name').textContent = `Name: ${data[count].name}`;
+            document.querySelector('#image').src = `${data[count].image}`
+            document.querySelector(`#image`).textContent = `Image link: ${data[count].image}`
+            
+            document.querySelector('#price').textContent = ` Price: $ ${data[count].price}`
+            document.querySelector('#description').textContent = `Description: ${data[count].description}`
+//Fetch and display product data from API.
+        }).catch((err)=>{
+            console.log(err);
+            alert("Sorry there was an error. ")
+        }).finally(()=>{
+            console.log("Load Successful")
+        });//catch error
 
 }
+const next = document. querySelector("#next");
+    next.addEventListener("click",function(){
+        call(count);
+        count = (count <21) ? ++count :0;// Add Next button
+    })
+const previous = document. querySelector("#previous");
+    previous.addEventListener("click",function(){
+        
+        count = (count>0) ? --count :21;
+        call(count);
+    })//Create navigation buttons for product display
+    
+
+
